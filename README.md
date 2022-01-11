ff135b2cdcc12bb4be30b6cfd35f582630cdc644
waldekglaz <waldekglaz@gmail.com>
2022-01-11 16:15:19 +0100

    readme

---
+++ README.md
@@ -0,0 +1,36 @@
+# Password Manager
+
+Password Manager app will save your login details and if you wish will generate very secure password for you. All data will be stored in txt file
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- Enter website address, email/username and password
+- Generate random secure password and can use immediately from the clipboard
+
+### Screenshot
+
+![](./Screenshot-password-man.jpg)
+
+### Links
+
+- [Code](https://github.com/waldekglaz/password-manager)
+
+## My process
+
+### Built with
+
+- Python 3
+- Tkinter
+- pyperclip
--- main.py
+++ main.py
@@ -1,5 +1,34 @@
 from tkinter import *
 from tkinter import messagebox
+import random
+import pyperclip
+
+
+# ---------------------------- PASSWORD GENERATOR ------------------------------- #
+def generate_password():
+    letters = [
+        "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s","t","u","v",
+        "w","x","y","z","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U",
+        "V","W","X", "Y", "Z",
+    ]
+    numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+    symbols = ["!", "#", "$", "%", "&", "(", ")", "*", "+"]
+
+    nr_letters = random.randint(8, 10)
+    nr_symbols = random.randint(2, 4)
+    nr_numbers = random.randint(2, 4)
+
+    password_letters = [random.choice(letters) for _ in range(nr_letters)]
+    password_symbols = [random.choice(symbols) for _ in range(nr_symbols)]
+    password_numbers = [random.choice(numbers) for _ in range(nr_numbers)]
+
+    password_list = password_numbers + password_letters + password_symbols
+    random.shuffle(password_list)
+
+    password = "".join(password_list)
+    password_input.insert(0, password)
+    pyperclip.copy(password)
+
 
 # ------------------------ SAVE PASSWORD -------------------------- #
 
@@ -21,6 +50,7 @@ def save():
             website_input.delete(0, END)
             password_input.delete(0, END)
 
+
 # ------------------------ UI SETUP -------------------------- #
 
 
@@ -28,7 +58,7 @@ window = Tk()
 window.title("Password manager")
 window.config(padx=50, pady=50)
 
-canvas = Canvas(heigh=200, width = 200)
+canvas = Canvas(heigh=200, width=200)
 padlock_img = PhotoImage(file="logo.png")
 canvas.create_image(100, 100, image=padlock_img)
 canvas.grid(column=1, row=0)
@@ -49,7 +79,7 @@ user_input.grid(column=1, row=2, columnspan=2, sticky="EW")
 password_input = Entry(width=21)
 password_input.grid(column=1, row=3, sticky="W")
 # ------ Buttons
-generate_btn = Button(text="Generate Password")
+generate_btn = Button(text="Generate Password", command=generate_password)
 generate_btn.grid(column=2, row=3, sticky="EW")
 add_btn = Button(width=36, text="Add", command=save)
 add_btn.grid(column=1, row=4, columnspan=2, sticky="EW")
